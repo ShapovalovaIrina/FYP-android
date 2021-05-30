e829b5c18a575b84e8fee9e02e1a605f0d9685d7
Shapovalova Irina <irishapovalova13@gmail.com>
2021-05-30 17:49:03 +0300

    UI polish

--- app/src/main/java/com/fyp/response/PetBody.java
+++ app/src/main/java/com/fyp/response/PetBody.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class PetBody {
     private String name;
+    private String source;
     private int type_id;
     private String breed;
     private String birth;
@@ -13,8 +14,9 @@ public class PetBody {
     private List<String> photos;
     private int shelter_id;
 
-    public PetBody(String name, int type_id, String breed, String birth, String description, String gender, String height, List<String> photos, int shelter_id) {
+    public PetBody(String name, String source, int type_id, String breed, String birth, String description, String gender, String height, List<String> photos, int shelter_id) {
         this.name = name;
+        this.source = source;
         this.type_id = type_id;
         this.breed = breed;
         this.birth = birth;
